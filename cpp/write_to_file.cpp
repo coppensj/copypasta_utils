4e9a8cd6f7731ccb90e902fa3c80725a9650b872
jcoppens <jcoppens@ghgsat.com>
2020-01-28 11:37:48 -0500

    Puts code from each example all together

--- cpp/write_to_file.cpp
+++ cpp/write_to_file.cpp
@@ -1,13 +1,12 @@
 #include <fstream>
 
 int main(){
-
-    const char* file1_name = "file1.csv";
-    const char* file2_name = "file2.csv";
-    std::ofstream f1, f2;
     int LEN = 10;
     float data[LEN] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    //ofstream example 1
+    const char* file1_name = "file1.csv";
+    std::ofstream f1;
     f1.open(file1_name);
     for(int i=0; i<LEN; i++){
         if(i == LEN - 1) f1 << data[i];
@@ -16,6 +15,9 @@ int main(){
     f1 << std::endl;
     f1.close();
     
+    //ofstream example 2
+    const char* file2_name = "file2.csv";
+    std::ofstream f2;
     f2.open(file2_name);
     for(int i=0; i<10; i++)
         f2 << i << ',' << data[i] << std::endl;
