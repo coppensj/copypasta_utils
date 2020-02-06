da82dda0ac4ba0cd7c4f8e17c96dba658e56d057
jcoppens <jcoppens@ghgsat.com>
2020-02-06 11:30:54 -0500

    Changes foldername from string to fs::path type and adds fs::current_path()

--- cpp/create_dir.cpp
+++ cpp/create_dir.cpp
@@ -1,15 +1,14 @@
 //make sure to add '-lstdc++fs' to LDFLAGS in the makefile
 
 #include <experimental/filesystem>
-#include <string>
 
 namespace fs = std::experimental::filesystem;
 
 int main(){
+    
+    fs::path foldername = "test_folder";
 
-    std::string foldername = "test_folder";
-
-    fs::create_directory(foldername);
+    fs::create_directory(fs::current_path() / foldername);
 
     return 0;
 }
