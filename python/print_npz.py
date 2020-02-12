f27002f8c3b89547742aa2e2bba0b27bdb9eb06e
jcoppens <jcoppens@ghgsat.com>
2020-02-12 15:13:27 -0500

    Removes histogram version. Separates loading data into separate file

--- python/print_npz.py
+++ python/print_npz.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-def print_npz(ipath, opath, name, VMIN, VMAX):
-    data = dict(np.load(ipath))
+def load_npz_data(ipath):
+    return dict(np.load(ipath))
+
+def print_npz(data, opath, name, VMIN, VMAX):
     for arr in data:
         num = [int(s) for s in arr.split('_') if s.isdigit()]
         if(num):
@@ -13,24 +15,3 @@ def print_npz(ipath, opath, name, VMIN, VMAX):
         print(savepath)
         plt.imsave(savepath, data[arr], cmap='gray', vmin=VMIN, vmax=VMAX)
 
-
-def print_npz_and_hist(ipath, opath, name, VMIN, VMAX):
-    data = np.load(INFILE)
-    for d in data:
-        print(name + "_" + d + ".png")
-        fig, (img, hist) = plt.subplots(nrows=1, ncols=2)
-        a = data[d]
-        
-        hist.set_xlim(VMIN, VMAX)
-        sns.distplot(a[np.nonzero(a)].flatten(), ax=hist, norm_hist=True)
-        sns.despine(ax=hist)
-        # hist.set_ylim(0, 0.1)
-        # hist.axvline(x=2885, ls='--', c='k')
-        # hist.axvline(x=2942, ls='--', c='k')
-
-        img.imshow(a, cmap='gray', vmin=VMIN, vmax=VMAX)
-        
-        fig.tight_layout()
-        fig.set_size_inches(w=11, h=5)
-        plt.savefig(opath + name + "_" + d + ".png")
-        plt.close()
