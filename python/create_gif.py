fcb99fd6d45e2bbbab693991978e6eaf504f49eb
jcoppens <jcoppens@ghgsat.com>
2020-02-13 11:39:42 -0500

    Adds wave plot example

--- python/create_gif.py
+++ python/create_gif.py
@@ -1,5 +1,13 @@
 import gif
 import matplotlib.pyplot as plt
+import numpy as np
+
+@gif.frame
+def gif_wave_plot(t):
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(2 * np.pi * (x - 0.01 * t))
+    plt.figure(figsize=(5, 3), dpi=100)
+    plt.plot(x, y, color='red')
 
 @gif.frame
 def gif_scatter_plot(data):
@@ -8,21 +16,25 @@ def gif_scatter_plot(data):
     plt.xlim((0, 100))
     plt.ylim((0, 100))
 
-def create_gif(data, plot, nframes):
+def create_gif(plot, data, nframes, name):
     frames = []
     for i in range(nframes):
         frame = plot(data[i])
         frames.append(frame)
-    gif.save(frames, "example.gif", duration=100)
+    gif.save(frames, f"{name}.gif", duration=100)
 
 if __name__ == "__main__":
+    
+    # scatter example
     from random import randint
-
     nframes = 50
     data = []
     for i in range(nframes):
         x = [randint(0, 100) for _ in range(10)]
         y = [randint(0, 100) for _ in range(10)]
         data.append((x, y))
+    create_gif(gif_scatter_plot, data, nframes, "scatter_example")
 
-    create_gif(data, gif_scatter_plot, nframes)
+    # wave plot example
+    times = [i for i in range(nframes)]
+    create_gif(gif_wave_plot, times, nframes, "wave_example")
